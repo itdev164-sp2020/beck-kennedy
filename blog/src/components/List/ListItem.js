583c1645107105c60143ca47ec68e5f17bdea81d
beck <kennedr6@gmatc.matc.edu>
2020-04-20 22:55:23 -0500

    completed activity 6

--- blog/src/components/List/ListItem.js
+++ blog/src/components/List/ListItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { BaseContainer } from '../BaseContainer'
 
 export const ListItem = ({ children, ...rest}) => (
-    <BaseContainer as ='li' {...rest}>
+    <BaseContainer as='li' {...rest}>
         {children}
     </BaseContainer>
 )
